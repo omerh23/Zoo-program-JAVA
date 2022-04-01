5a24cc13389c65ad183fca2e4b78530db00477bc
Vlad <69718459+VladFearo@users.noreply.github.com>
2022-04-01 18:35:43 +0300

    added javaDoc

--- src/food/EFoodType.java
+++ src/food/EFoodType.java
@@ -1,5 +1,9 @@
 package food;
-
+/**
+ * An enum organizing all the food types there are
+ *
+ * @authors  Vladislav Shevtsov id: 322162553; Omer Halfon id: 315429951
+ */
  public enum EFoodType {
 	 
 	 MEAT,
--- src/food/IEdible.java
+++ src/food/IEdible.java
@@ -1,7 +1,14 @@
 package food;
-
+/**
+ * An interface organizing the food types and defines the getFoodType method
+ *
+ * @authors  Vladislav Shevtsov id: 322162553; Omer Halfon id: 315429951
+ * @see     EFoodType
+ */
 public interface IEdible {
-	
+	/*
+	 * A declaration of the getFoodtype method
+	 * */
 	public EFoodType getFoodtype();
 	
 
